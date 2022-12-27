82cbe5536657bcd7893cea1840ec04cb2af8543f
Paulo Ricardo Caldi <pcaldi@gmail.com>
2022-12-27 20:43:55 -0300

    Componente Button

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -4,6 +4,7 @@ import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
 import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 export function SignIn() {
   return(
@@ -36,6 +37,8 @@ export function SignIn() {
     placeholder="Senha"
     secureTextEntry
     />
+
+    <Button title="Acessar"/>
 
     </VStack>
   );

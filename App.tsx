868f292db6c80de6eee631a5aa4324d6f0375603
Paulo Ricardo Caldi <pcaldi@gmail.com>
2022-12-27 18:19:01 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import {  StatusBar } from 'react-native';
+import {NativeBaseProvider} from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
+import { THEME } from './src/theme'
+import { SignIn } from '@screens/SignIn';
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+       {fontsLoaded ? <SignIn/> : <Loading/>}
+    </NativeBaseProvider>
+  );
+}
+

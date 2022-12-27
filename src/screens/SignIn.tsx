5fe3e35a18245a20143acd7c4e78cd89f02acb81
Paulo Ricardo Caldi <pcaldi@gmail.com>
2022-12-27 19:19:30 -0300

    Componente Heading

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Text, Center } from 'native-base'
+import { VStack, Image, Text, Center, Heading } from 'native-base'
 
 import LogoSvg from '@assets/logo.svg';
 
@@ -19,6 +19,11 @@ export function SignIn() {
       <Text color="gray.100" fontSize="sm">
          Treine sua mente e seu corpo
       </Text>
+    </Center>
+    <Center>
+      <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+        Acesse sua conta
+      </Heading>
     </Center>
 
     </VStack>

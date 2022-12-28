abd0890a3f762d0ab95e8d7ae8364c67ebdfcb18
Paulo Ricardo Caldi <pcaldi@gmail.com>
2022-12-27 23:12:59 -0300

    Utilizando Variantes/Finalizando SignIn

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,22 +2,25 @@ import { Button as ButtonNativeBase, IButtonProps, Text } from 'native-base';
 
 type Props = IButtonProps & {
   title: string;
+  variant?: 'solid' | 'outline';
 }
 
-export function Button({title, ...rest}: Props) {
+export function Button({title, variant = 'solid' , ...rest}: Props) {
   return(
     <ButtonNativeBase 
     w="full"
     h={14}
-    bg="green.700"
+    bg={variant === "outline" ? "transparent" : "green.700"}
+    borderWidth={variant === "outline" ? 1 : 0}
+    borderColor="green.500"
     rounded="sm"
     _pressed={{
-      bg: "green.500"
+      bg: variant === "outline" ? "gray.500": "green.500"
     }}
     {...rest}
     >
       <Text 
-        color="white"
+        color={variant === "outline" ? "green.500" : "white"}
         fontSize="sm"
         fontFamily="heading"
        >

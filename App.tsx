7ae50f553991453c7086c5c038d0966ced36649b
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-02 16:48:09 -0300

    Mudando Tema App

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import {  StatusBar } from 'react-native';
 import {NativeBaseProvider} from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { Routes } from './src/routes';
+
 import { Loading } from '@components/Loading';
 import { THEME } from './src/theme'
-import { SignUp } from '@screens/SignUp';
 
 
 export default function App() {
@@ -16,7 +17,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-       {fontsLoaded ? <SignUp/> : <Loading/>}
+       {fontsLoaded ? <Routes/> : <Loading/>}
     </NativeBaseProvider>
   );
 }

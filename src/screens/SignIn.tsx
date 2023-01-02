a484ae73e7b1d9463d72749d62325cb751a5249c
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-02 17:35:36 -0300

    Navegação de autenticação

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,12 +1,22 @@
+import { useNavigation } from '@react-navigation/native';
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
+import { AuthNavigatorRoutesProps } from '@routes/auth.routes'
+
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function SignIn() {
+
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  function handleNewAccount() {
+    navigation.navigate('signUp')
+  }
+
   return(
   <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
     <VStack flex={1} px={10} pb={16}>
@@ -47,6 +57,7 @@ export function SignIn() {
       <Button 
         title="Criar conta" 
         variant="outline"
+        onPress={handleNewAccount}
       />
     </Center>
 

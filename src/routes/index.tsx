0e81c89949b7311e6ae443d98ea5ef084757c472
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-02 20:22:05 -0300

    Type Routes App

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import { Box, useTheme } from "native-base";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
 import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
+
 
 
 
@@ -13,7 +15,7 @@ export function Routes(){
   return (
     <Box flex={1} bg="gray.700">
     <NavigationContainer theme={theme}>
-      <AuthRoutes />
+      <AppRoutes />
     </NavigationContainer>
     </Box>
   )

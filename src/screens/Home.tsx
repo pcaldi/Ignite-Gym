a4f7bc5d15fae13c4224339b1f7d7912fb18b5c5
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-03 14:24:24 -0300

    Estilizando Pressed

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,18 @@
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import { VStack } from 'native-base';
+import { HStack, VStack } from 'native-base';
 
 export function Home(){
   return(
     <VStack flex={1}>
       <HomeHeader/>
-      <Group name="costas"/>
+
+      <HStack>
+      <Group name="costa"/>
+      <Group name="ombro"/>
+      
+      </HStack>
+
     </VStack>
   );
 }

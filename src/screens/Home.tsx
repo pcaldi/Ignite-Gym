7882f65b9808b20460a3569ce53ba4fd74ce8508
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-03 15:16:57 -0300

    Utilizando Flatlist

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,29 +1,34 @@
 import { useState } from 'react';
-import { HStack, VStack } from 'native-base';
+import { HStack, VStack, FlatList} from 'native-base';
 
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 
 export function Home(){
   const [groupSelected, setGroupSelected] = useState('costa');
+  const [groups, setGroups] = useState(['Costas', 'Biceps', 'Triceps', 'Ombro']);
 
   return(
     <VStack flex={1}>
       <HomeHeader/>
 
-      <HStack>
-        <Group 
-          name="costa" 
-          isActive={groupSelected === "costa"}
-          onPress={() => setGroupSelected("costa")}
-        />
-        <Group 
-          name="ombro" 
-          isActive={groupSelected === "ombro"}
-          onPress={() => setGroupSelected("ombro")}
-        />
-      
-      </HStack>
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <Group 
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
 
     </VStack>
   );

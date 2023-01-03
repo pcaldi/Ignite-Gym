335f449c2c1568c4b006fddd7647713850c57cb4
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-03 14:19:57 -0300

    Component Group/Pressable

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,12 @@
+import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import {Center, Text, VStack} from 'native-base';
+import { VStack } from 'native-base';
 
 export function Home(){
   return(
     <VStack flex={1}>
       <HomeHeader/>
+      <Group name="costas"/>
     </VStack>
   );
 }

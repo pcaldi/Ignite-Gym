e8bfbfaabb7f8f17836206ccfc875b7ec4079119
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-03 15:24:46 -0300

    Header da lista de exercícios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { HStack, VStack, FlatList} from 'native-base';
+import { HStack, VStack, FlatList, Heading, Text} from 'native-base';
 
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
@@ -28,8 +28,18 @@ export function Home(){
         my={10}
         maxH={10}
       />
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
 
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
 
+      </VStack>
     </VStack>
   );
 }

47dc58cdbb2a6c8fc41fdb590db6db27a812ce2a
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-03 15:04:31 -0300

    Exibindo grupo selecionado

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,27 @@
-import { Group } from '@components/Group';
-import { HomeHeader } from '@components/HomeHeader';
+import { useState } from 'react';
 import { HStack, VStack } from 'native-base';
 
+import { Group } from '@components/Group';
+import { HomeHeader } from '@components/HomeHeader';
+
 export function Home(){
+  const [groupSelected, setGroupSelected] = useState('costa');
+
   return(
     <VStack flex={1}>
       <HomeHeader/>
 
       <HStack>
-      <Group name="costa"/>
-      <Group name="ombro"/>
+        <Group 
+          name="costa" 
+          isActive={groupSelected === "costa"}
+          onPress={() => setGroupSelected("costa")}
+        />
+        <Group 
+          name="ombro" 
+          isActive={groupSelected === "ombro"}
+          onPress={() => setGroupSelected("ombro")}
+        />
       
       </HStack>
 

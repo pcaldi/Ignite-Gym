d14cee20735a6da5d1acefa0dbc921511d309791
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-03 18:16:32 -0300

    ListEmptyComponent

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import {Heading, VStack, SectionList } from 'native-base';
+import {Heading, VStack, SectionList, Text } from 'native-base';
 
 
 import { ScreenHeader } from '@components/ScreenHeader';
@@ -34,6 +34,13 @@ export function History(){
         </Heading>
       )}
       px={8}
+      contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center'}}
+      ListEmptyComponent={() => (
+        <Text color="gray.100" textAlign="center">
+          Não há exercícios registrados ainda. {'\n'}
+          Vamos praticar exercícios hoje?
+        </Text>
+      )}
     />
     </VStack>
   );

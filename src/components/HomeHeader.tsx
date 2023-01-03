e66bde208132822b370e9170addbf42040ef4fd2
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-03 13:28:19 -0300

    Estilizando HomeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,12 +2,12 @@ import { Heading, VStack , HStack, Text } from "native-base";
 
 export function HomeHeader(){
   return(
-    <HStack>
+    <HStack bg="gray.600" pt={16} pb={6} px={8} alignItems="center">
       <VStack>
-        <Text color="gray.100"> 
+        <Text color="gray.100" fontSize="md">  
           Olá,
         </Text>
-        <Heading color="gray.100">
+        <Heading color="gray.100" fontSize="md">
           Paulo Caldi
         </Heading>
       </VStack>

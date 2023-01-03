508d83d7b20e93cb37bd5112825adbffef38f449
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-03 13:38:28 -0300

    Componente UserPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,16 @@
 import { Heading, VStack , HStack, Text } from "native-base";
 
+import { UserPhoto } from "./UserPhoto";
+
 export function HomeHeader(){
   return(
     <HStack bg="gray.600" pt={16} pb={6} px={8} alignItems="center">
+      <UserPhoto
+      source={{ 'uri': 'https://github.com/pcaldi.png'}}
+      alt="Imagem do usuário"
+      size={16}
+      mr={4}
+      />
       <VStack>
         <Text color="gray.100" fontSize="md">  
           Olá,

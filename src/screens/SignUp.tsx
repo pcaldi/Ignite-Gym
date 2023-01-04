a6cec82a649d20cdcdba701af35480d513ef406a
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-04 16:49:24 -0300

    Utilizando Schemas Validação com YUP

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,6 +1,8 @@
 import { useNavigation } from '@react-navigation/native';
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 import { useForm, Controller} from 'react-hook-form';
+import { yupResolver} from "@hookform/resolvers/yup";
+import * as yup from 'yup';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -16,9 +18,17 @@ type FormDataProps = {
   password_confirm: string;
 }
 
+const signUpSchema = yup.object({
+  name: yup.string().required('Informe o nome'),
+  email: yup.string().required('Informe o e-mail').email('E-mail inválido')
+
+});
+
 export function SignUp() {
 
-  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>();
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema)
+  });
   
   const navigation = useNavigation()
   function handleGoBack(){
@@ -54,9 +64,6 @@ export function SignUp() {
       <Controller
         control={control}   
         name='name'
-        rules={{
-          required: 'Informe o nome.'
-        }}
         render={({field : {onChange , value}}) => (
         <Input 
           placeholder="Nome" 
@@ -69,13 +76,6 @@ export function SignUp() {
       <Controller
         control={control}   
         name='email'
-        rules={{
-          required: 'Informe o email.',
-          pattern: {
-            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-            message: 'E-mail inválido.'
-          }
-        }}
         render={({field : {onChange , value}}) => (
         <Input 
           placeholder="Email" 

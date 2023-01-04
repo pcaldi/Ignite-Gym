9980c6b0e189bda3a4f200c3f6e307e85e7945f2
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-04 14:48:00 -0300

    Tipando os dados do formulário

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -9,17 +9,25 @@ import BackgroundImg from '@assets/background.png';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
+
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+}
+
 export function SignUp() {
 
-  const { control, handleSubmit } = useForm();
+  const { control, handleSubmit } = useForm<FormDataProps>();
   
   const navigation = useNavigation()
   function handleGoBack(){
     navigation.goBack();
   }
 
-  function handleSignUp(data: any) {
-    console.log(data);
+  function handleSignUp({name, email, password, password_confirm}: FormDataProps) {
+    console.log({name, email, password, password_confirm});
   }
 
   return(

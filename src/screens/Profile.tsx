fc6bba8b7966c41f7d39e9aca83de3501ff6f5d6
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-04 12:47:26 -0300

    Utilizando mensagem toast

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { TouchableOpacity, Alert } from 'react-native';
-import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from 'native-base';
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
 
@@ -16,6 +16,8 @@ const PHOTO_SIZE = 33;
 export function Profile(){
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState('https://github.com/pcaldi.png');
+  
+  const toast = useToast()
 
   async function handleUserPhotoSelect(){
     setPhotoIsLoading(true);
@@ -34,7 +36,11 @@ export function Profile(){
         const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
        
         if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5){
-          return Alert.alert("Essa imagem é muito grande. Escolha uma de até 5MB.")
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            placement: "top",
+            bg: 'red.500'
+          });
         }
 
 

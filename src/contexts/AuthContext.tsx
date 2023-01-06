ed1fcac71176bfe43d7bf452509123dc1bcfbaa9
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-06 20:57:17 -0300

    Buscando dados backend

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,13 @@
 import { UserDTO } from "@dtos/UserDTO";
 
+import { api } from "@services/api";
+
 import { createContext, ReactNode, useState } from "react";
+import { Use } from "react-native-svg";
 
 export type AuthContextDataProps ={
   user: UserDTO;
-  signIn: (email: string, password: string) => void;
+  signIn: (email: string, password: string) => Promise<void>;
 }
 
 type AuthContextProviderProps = {
@@ -13,20 +16,17 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps){
-  const [ user, setUser ] = useState(
-    {
-      id: '1',
-      name: 'Paulo Caldi',
-      email: 'paulo@email.com',
-      avatar: 'paulo.png'
-  });
-  function signIn(email: string, password: string){
-    setUser({
-      id: '',
-      name: '',
-      email,
-      avatar: '',
-    });
+  const [ user, setUser ] = useState<UserDTO>({} as UserDTO);
+
+  async function signIn(email: string, password: string){
+    try {
+      const { data } = await api.post('/sessions', { email, password})
+      if ( data.user){
+       setUser(data.user)
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   return (

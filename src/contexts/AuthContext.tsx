f2433a5af4de12c6204289334cfcc8f4d103c911
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-01-06 18:03:44 -0300

    Utilizando useState no contexto

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { UserDTO } from "@dtos/UserDTO";
 
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 export type AuthContextDataProps ={
   user: UserDTO;
@@ -12,15 +12,14 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps){
-  return (
-    <AuthContext.Provider value={{
-      user: {
+  const [ user, setUser ] = useState({
         id: '1',
-        name: 'Paulo Ricardo Caldi',
+        name: 'Paulo Caldi',
         email: 'paulo@email.com',
         avatar: 'paulo.png'
-      }
-    }}>
+  })
+  return (
+    <AuthContext.Provider value={{ user }}>
       {children}
     </AuthContext.Provider>
   );
